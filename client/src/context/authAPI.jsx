fb81718e87fad7de9dc6def21ea2cae012073de0
sr931662 <83828392+sr931662@users.noreply.github.com>
2025-12-18 00:26:14 +0530

    Update authAPI.jsx

--- client/src/context/authAPI.jsx
+++ client/src/context/authAPI.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://localhost:5000/api',
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'https://utkarsh-server-807521575693.us-east1.run.app/api',
+  baseURL: 'https://utkarsh-server-807521575693.us-east1.run.app/api',
   withCredentials: true
 });
 
